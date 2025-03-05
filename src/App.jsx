13924d16caaaeaaec914ce76f0ada9f9aeede7c6
Zander <zandermarenberg@gmail.com>
2025-03-05 18:46:43 -0500

    react with tailwind install

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./index.css"
-
+import Home from "./pages/Home"
 function App() {
   return (
-    <div className="min-h-screen bg-blue-500 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-white">
-        Hello Tailwind! Poop
-      </h1>
-    </div>
+    <>
+      <Home />
+    </>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-blue-500 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-center text-white">Home Page</h1>
+    </div>
+  )
+}
+
