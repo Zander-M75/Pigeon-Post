3d4f36f26e972f607a7f9e244c76e906e4e4e87c
Zander <zandermarenberg@gmail.com>
2025-03-05 18:43:50 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./index.css"
+
+function App() {
+  return (
+    <div className="min-h-screen bg-blue-500 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">
+        Hello Tailwind! Poop
+      </h1>
+    </div>
+  )
+}
+
+export default App
