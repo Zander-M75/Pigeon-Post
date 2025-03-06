67354d3809ca60e20c82796b48084165b2a95b82
Zander <zandermarenberg@gmail.com>
2025-03-05 19:12:56 -0500

    header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./index.css"
-import Home from "./pages/Home"
+import Layout from './components/layout/Layout'
+
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Layout>
+      {/* Your routes or content will go here */}
+      <div className="container mx-auto px-4 py-8">
+        {/* Example content */}
+        <h1 className="text-4xl font-bold mb-4">Welcome to Pigeon Post</h1>
+        <p className="text-gray-600">Where every package has wings!</p>
+      </div>
+    </Layout>
   )
 }
 
