2f6fb9060e05255da8c63c54e5f772d670384a21
Zander <zandermarenberg@gmail.com>
2025-03-10 11:13:40 -0400

    hero styling changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,13 @@ import officeClipart from "../assets/Home/office-clipart.png";
 export default function Home() {
   return (
     <div className="bg-[#161616] h-screen w-screen p-8">
-      <div className="flex w-full h-screen items-center">
+      <div className="flex w-full h-auto items-center justify-around">
         {/* Left div - Text content */}
-        <div className="w-1/2 p-8 flex flex-col gap-6">
-          <h4 className="text-white text-2xl font-bold">Pigeon Post</h4>
-          <h2 className="text-white text-5xl font-bold">Efficient Pigeon Logistics</h2>
-          <p className="text-white text-sm">Discover the fastest way to send and receive messages with our innovative pigeon delivery service.</p>
-          <div className="flex flex-row items-center justify-evenly">
+        <div className="w-1/2 p-8 flex flex-col gap-6 ml-24">
+          <h4 className="text-white text-xl">~ Pigeon Post</h4>
+          <h2 className="text-white text-6xl font-bold">Efficient<br/> Pigeon<br/> Logistics</h2>
+          <p className="text-white text-m max-w-lg">Discover the fastest way to send and receive messages with our innovative pigeon delivery service.</p>
+          <div className="flex flex-row items-center justify-start gap-4">
             <BlackButton href="/signup">Sign Up Now</BlackButton>
             <BlackButton href="/login">Log In</BlackButton>
           </div>
